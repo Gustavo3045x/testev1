bc88fcb3dfd16842881faf18ede78af26e98bf1a
Aluno Caxias Temporário <senacrs.aulas@gmail.com>
2024-01-22 21:45:01 -0300

    Update 24. 1 .22

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,11 @@
+﻿float nm,sh,tt;
+
+Console.Write("escreva seu nome: ");
+nm = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\n\ndigite su email: ");
+sh = float.Parse(Console.ReadLine());
+
+tt = nm * sh;
+
+Console.WriteLine("total: " + tt);
